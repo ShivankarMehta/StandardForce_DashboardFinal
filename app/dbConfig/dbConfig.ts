818b3dde2328cdc6b6054ee77aa52093e08fdd45
i-m-abbhay <20je0018@me.iitism.ac.in>
2024-08-17 09:36:48 +0530

    Starting the optimisation

--- app/dbConfig/dbConfig.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('tomsms_db', 'root', process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-export default sequelize;
